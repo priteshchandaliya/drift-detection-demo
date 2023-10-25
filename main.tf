f49267545ecb85223d97d08d49588739485bbe39
Pritesh Chandaliya <pritesh@aporeto.com>
2023-10-25 15:40:03 -0700

    add terraform AWS provider and S3  bucket

--- main.tf
+++ main.tf
@@ -2,27 +2,29 @@ data "aws_caller_identity" "current" {}
 
 
 locals {
- account_id = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 }
 
 
 output "account_id" {
- value = local.account_id
+  value = local.account_id
 }
 
 
 resource "random_id" "rando" {
- byte_length = 8
+  byte_length = 8
 }
 
 
 resource "aws_s3_bucket" "my-bucket" {
- bucket = "${local.account_id}-my-s3-bucket-${random_id.rando.hex}"
+  bucket = "${local.account_id}-my-s3-bucket-${random_id.rando.hex}"
 
 
- tags = {
-   Name        = "My bucket"
-   Environment = "Dev"
- }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    yor_name    = "my-bucket"
+    yor_trace   = "78e07d5c-9f8e-4451-894c-4337a8dfbdbd"
+  }
 }
 
